53f7db09e597bd372418f4cd77b4cd6fec974f8d
yd072 <luiquirong@gmail.com>
2024-12-24 23:00:54 +0800

    Update yx_ips.py

--- yx_ips.py
+++ yx_ips.py
@@ -10,11 +10,8 @@ headers = {
 
 # 定义五个网址
 urls = [
-    "https://cf.090227.xyz/",
     "https://stock.hostmonit.com/CloudFlareYes",
-    "https://ip.164746.xyz/",
-    "https://monitor.gacjie.cn/page/cloudflare/ipv4.html",
-    "https://345673.xyz/"
+
 ]
 
 # 解析延迟数据的正则表达式
