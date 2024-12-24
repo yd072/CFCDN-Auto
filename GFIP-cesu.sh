7271a8d599a726756d6bb37265ffa1b81b3ba892
yd072 <luiquirong@gmail.com>
2024-12-24 10:16:07 +0800

    Update GFIP-cesu.sh

--- GFIP-cesu.sh
+++ GFIP-cesu.sh
@@ -22,11 +22,11 @@ else
     echo "CloudflareST文件已存在，跳过下载步骤。"
 fi
 
-# 7. 执行 CloudflareST 进行测速
+#  执行 CloudflareST 进行测速
 echo "======================运行 CloudflareSpeedTest ========================="
 "${CFST_DIR}/CloudflareST" -tp 443 -f "${CFST_DIR}/ip.txt" -n 500 -dn 5 -tl 250 -tll 10 -o "${CFST_DIR}/ip.csv" -url "$URL"
 
-# 8. 从 sg.csv 文件中筛选下载速度高于 5 的 IP地址，并删除重复的 IP 地址行，生成 sgcs.txt
+#  从 sg.csv 文件中筛选下载速度高于 5 的 IP地址，并删除重复的 IP 地址行，生成 sgcs.txt
 echo "==================筛选下载速度高于 10mb/s 的IP地址并去重===================="
 awk -F, '!seen[$1]++' "${CFST_DIR}/ip.csv" | awk -F, 'NR>1 && $6 > 10 {print $1 "#" $6 "mb/s"}' > "${CFST_DIR}/gf-ip.txt"
 
